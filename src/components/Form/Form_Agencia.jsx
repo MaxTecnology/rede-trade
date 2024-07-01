7f24ad8cc6f94f10264336a880e7124ea057f89d
MaxTecnology <maxsouza150@hotmail.com>
2024-07-01 11:28:55 -0300

    Fix cadastro associado

--- src/components/Form/Form_Agencia.jsx
+++ src/components/Form/Form_Agencia.jsx
@@ -1,22 +1,72 @@
 import FormSelect from "./formItens/FormSelect";
 import FormInput from "./formItens/FormInput";
 import FormPlano from "./formItens/FormPlano";
+import { useState } from "react";
 
 const Form_Agencia = ({ form, type }) => {
-    return (<>
-        <FormPlano type={type} form={form} />
-        <FormSelect required form={form} name="formaPagamento" label="Forma de pagamento do Plano" placeholder="Selecionar" items={[
-            { value: 100, label: "Permuta" },
-            { value: 0, label: "Dinheiro" },
-            { value: 50, label: "Permuta / Dinheiro" },
-        ]} />
-        <FormSelect required form={form} name="dataVencimentoFatura" label="Data Vencimento Fatura" placeholder="Selecionar" items={[
-            { value: 10, label: "10" },
-            { value: 20, label: "20" },
-            { value: 30, label: "30" },
-        ]} />
-        <FormInput required form={form} name="nomeFranquia" label="Nome da Agência" placeholder="Franquia" divClassName={""} disabled />
-    </>)
+  const [pagamentoValue, setPagamento] = useState(100);
+  return (
+    <>
+      <FormPlano type={type} form={form} />
+      <FormSelect
+        required
+        form={form}
+        name="formaPagamento"
+        label="Forma de pagamento do Plano"
+        placeholder="Selecionar"
+        onChange={(e) => {
+          setPagamento(e.target.value);
+        }}
+        items={[
+          { value: 100, label: "Permuta" },
+          { value: 0, label: "Dinheiro" },
+          { value: 50, label: "Permuta / Dinheiro" },
+        ]}
+      />
+      {pagamentoValue == 50 && (
+        <>
+          <FormInput
+            required
+            form={form}
+            name="dinheiro"
+            label="Dinheiro"
+            placeholder="R$ 0,00"
+            divClassName={""}
+          />
+          <FormInput
+            required
+            form={form}
+            name="permulta"
+            label="Permulta"
+            placeholder="R$ 0,00"
+            divClassName={""}
+          />
+        </>
+      )}
+
+      <FormSelect
+        required
+        form={form}
+        name="dataVencimentoFatura"
+        label="Data Vencimento Fatura"
+        placeholder="Selecionar"
+        items={[
+          { value: 10, label: "10" },
+          { value: 20, label: "20" },
+          { value: 30, label: "30" },
+        ]}
+      />
+      <FormInput
+        required
+        form={form}
+        name="nomeFranquia"
+        label="Nome da Agência"
+        placeholder="Franquia"
+        divClassName={""}
+        disabled
+      />
+    </>
+  );
 };
 
 export default Form_Agencia;
--- src/components/Form/formItens/FormSelect.jsx
+++ src/components/Form/formItens/FormSelect.jsx
@@ -1,35 +1,65 @@
-import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { cn } from "@/lib/utils";
 
-const FormSelect = ({ form, name, label, required, placeholder, items, options, empty }) => {
-    return (<>
-        <FormField
-            control={form.control}
-            name={name}
-            render={({ field }) => (
-                <FormItem className="form-group position-relative">
-                    <div className="flex gap-2 items-center">
-                        <FormLabel className={cn("pl-2 text-md", required && "required")}>{label}</FormLabel>
-                        <FormMessage />
-                    </div>
-                    <FormControl>
-                        <select
-                            {...field}
-                            className="!mt-0"
-                        >
-                            <option value={empty ? "null" : ""} disabled>{placeholder}</option>
-                            {items ? items.map((option) => (
-                                <option key={option.value} value={option.value}>
-                                    {option.label}
-                                </option>
-                            )) : null}
-                            {options}
-                        </select>
-                    </FormControl>
-                </FormItem>
-            )}
-        />
-    </>)
+const FormSelect = ({
+  form,
+  name,
+  label,
+  required,
+  placeholder,
+  items,
+  options,
+  empty,
+  onChange,
+}) => {
+  return (
+    <>
+      <FormField
+        control={form.control}
+        name={name}
+        render={({ field }) => (
+          <FormItem className="form-group position-relative">
+            <div className="flex gap-2 items-center">
+              <FormLabel className={cn("pl-2 text-md", required && "required")}>
+                {label}
+              </FormLabel>
+              <FormMessage />
+            </div>
+            <FormControl>
+              <select
+                {...field}
+                className="!mt-0"
+                onChange={(e) => {
+                  field.onChange(e);
+                  if (onChange) {
+                    onChange(e);
+                  }
+                }}
+              >
+                <option value={empty ? "null" : ""} disabled>
+                  {placeholder}
+                </option>
+                {items
+                  ? items.map((option) => (
+                      <option key={option.value} value={option.value}>
+                        {option.label}
+                      </option>
+                    ))
+                  : null}
+                {options}
+              </select>
+            </FormControl>
+          </FormItem>
+        )}
+      />
+    </>
+  );
 };
 
 export default FormSelect;
